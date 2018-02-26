4e60867a6caefd03844b49d35721ab45f303c7f7
Damien <dlim7@hawaii.edu>
2018-02-25 22:51:36 -1000

    Task 3
    Done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
@@ -64,3 +71,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
